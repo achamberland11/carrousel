b32fca99432f088edf6bb2d7d49d3ee6a6f438d7
Virgile <112096529+achamberland11@users.noreply.github.com>
2023-05-02 11:19:53 -0400

    Add files via upload

--- carrousel.php
+++ carrousel.php
@@ -1,17 +1,17 @@
 <?php
 /**
  * Plugin Name: Carrousel
- * Author: Antoine Chamberland
- * Author uri: https://github.com/achamberland11
- * Description: Permet d'afficher les images d'une galerie dans une boîte modale navigable
+ * Author: Nicolas Carriere
+ * Author uri: https://github.com/nicolasDesrosiers
+ * Description: Permet d'afficher les images d'une galerie dans une boîte modale naviguable
  */
 
-    /* style.css pour formater et animer le caroussel */
-    /* carrousel.js pour contrôler le carrousel */
-    /* Boîte modale qui contiendra le carrousel */
+ /* style.css  formater et animer le carrousel */
+ /* carrousel.js  pour contrôler le carrousel */
+ /* boîte modale qui contiendra le carrousel */
 
-/*
-    filemtime() // retourne en milliseconde le temps de la dernière sauvegarde
+ /*
+ filemtime() // retourne en milliseconde le temps de la dernière sauvegarde
 plugin_dir_path() // retourne le chemin du répertoire du plugin
 __FILE__ // une constante contenant le chemin du fichier en train de s'exécuter
 wp_enqueue_style() // Intègre le link:css dans la page
@@ -40,11 +40,14 @@ add_action('wp_enqueue_scripts', 'enfiler_script_css' );
 
 function genere_boite()
 {
-    return '<button class="carrousel__ouvrir">Ouvrir Carrousel</button>
+    return '
+            
             <div class="carrousel">
+                <i class="fleche__gauche"></i>
                 <button class="carrousel__x">X</button>
                 <figure class="carrousel__figure"></figure>
                 <form class="carrousel__form"></form>
+                <i class="fleche__droite"></i>
             </div>';
 }
 add_shortcode('carrousel', 'genere_boite');
---
+++ js/carrousel.js
@@ -0,0 +1,142 @@
+(function(){
+ 
+   /* -------------------------------------------------------- Variable du carrousel */
+   let carrousel  = document.querySelector(".carrousel")
+   let carrousel__x = document.querySelector(".carrousel__x")
+   let carrousel__figure = document.querySelector(".carrousel__figure")
+   let carrousel__form = document.querySelector(".carrousel__form")
+   let fleche__gauche = document.querySelector(".fleche__droite")
+   let fleche__droite = document.querySelector(".fleche__gauche")
+   
+   /* -------------------------------------------------------- Variable de la galerie */
+   let galerie = document.querySelector(".wp-block-gallery")
+   let galerie__img = galerie.querySelectorAll("img")
+   // console.log("galerie__img: " + galerie__img.length)
+   // console.log(carrousel.tagName)
+   /** ---------------------------------------------------- Positionnement de l'img active du carrousel */
+   
+   let index = 0 
+   let dernier__index = -1
+   let position = 0 // permet d'indexer les images de la galerie et 
+
+
+   /** 
+    * ajouter_img_dans_carrousel
+    * Ajouter l'ensemble des images de la galerie dans la boîte modale carrousel
+    */
+   
+   for (const elm of galerie__img)
+   {
+    elm.dataset.index = position
+    elm.addEventListener('mousedown',function(){
+
+      if(carrousel.classList.contains('carrousel--activer') == false){
+        carrousel.classList.add('carrousel--activer')
+      }
+      index = this.dataset.index
+      afficher_image(index)                      
+      console.log(index)
+    })
+
+    creation_img_carrousel(elm)
+    creation_radio_carrousel()
+   }
+ 
+
+   
+   
+   /* ----------------------------------------------------  fermer boîte modale */
+   carrousel__x.addEventListener('mousedown', function(){
+       console.log('fermer la boîte modale')
+       carrousel.classList.remove('carrousel--activer')
+
+   })
+
+   /* ----------------------------------------------------  changer img avec fleches */
+
+   fleche__gauche.addEventListener('mousedown', function(){
+    //console.log('changer photo a gauche')
+    for( const elm of galerie__img)
+    {
+      if(index <= 0){
+        index = galerie__img.length
+      } else { index = index-1 }
+    }     
+    afficher_image(index)
+
+  })
+
+  fleche__droite.addEventListener('mousedown', function(){
+    //console.log('changer photo a droite')
+    for( const elm of galerie__img)
+    {
+      if(index >= galerie__img.length){
+        index = 0
+      } else{index = index+1}
+    }  
+    afficher_image(index)
+
+  })
+
+
+
+
+ 
+   function creation_img_carrousel(elm){
+
+     //console.log(elm.getAttribute('src'))
+     let img = document.createElement('img')
+     //img.setAttribute('src', elm.getAttribute('src'))
+     img.src = elm.src
+     img.classList.add('carrousel__img')
+     //console.log (img.getAttribute('src'))
+     carrousel__figure.appendChild(img)
+     
+   }
+
+
+   /**
+    * Création d'un radio-bouton
+    */
+   
+   function creation_radio_carrousel(){
+
+   let rad = document.createElement('input')
+   console.log(rad.tagName)
+   rad.setAttribute('type', 'radio')
+   rad.setAttribute('name', 'carrousel__rad')
+   rad.classList.add('carrousel__rad')
+   rad.dataset.index = position // permet d'asscocier chacun des radio bouton a l'image du carrousel 
+   position++ // incrémentation de 1 
+   carrousel__form.appendChild(rad)
+
+   /** afficher image / Transition */
+
+   rad.addEventListener('mousedown', function(){
+    afficher_image(index)
+    console.log(this.dataset.index)
+    index = this.dataset.index
+   })
+   }
+
+   function afficher_image(index){
+
+    if(dernier__index != -1){
+      //carrousel__figure.children[dernier__index].style.opacity = 0
+      carrousel__figure.children[dernier__index].classList.remove('carrousel__img--activer')
+      //carrousel__form.children[dernier__index].checked = false // A completer ...
+    } 
+    //carrousel__figure.children[this.dataset.index].style.opacity = 1
+    carrousel__figure.children[index].classList.add('carrousel__img--activer')
+    
+    dernier__index = index
+
+
+   }
+
+   /** Permet de vérifier si la classe "carrousel--activer se trrouve dans la liste des classes du carrousel"
+    * carrousel.classList.contain('carrousel--activer')
+    */
+
+
+   })()
